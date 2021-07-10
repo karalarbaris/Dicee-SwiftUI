3058a01d413c16df69e53350ca11d402b119bbb2
Baris <karalarbaris@gmail.com>
2021-07-10 22:32:42 +0200

    7. SwiftUI Dicee Part 1 - Designing a Layout using Spacers and Subviews

--- Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/ContentView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+                 
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                 HStack {
+                    Spacer()
+                    DiceView(imageName: "dice1")
+                    Spacer()
+                    DiceView(imageName: "dice3")
+                    Spacer()
+                    
+                }
+                .padding()
+                Spacer()
+                Button(action: {
+                    
+                }, label: {
+                    Text("Roll")
+                        .font(.system(size: 40))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                })
+                .background(Color.red)
+
+            }
+        }
+        
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    
+    var imageName: String
+    
+    var body: some View {
+        
+        Image(imageName)
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .frame(width: 100, height: 100, alignment: .center)
     }
 }
